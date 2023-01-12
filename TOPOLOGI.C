1526efa5eb31adfabb576de0634a4a7c78d566cb
sandraanieeby <116794078+sandraanieeby@users.noreply.github.com>
2023-01-11 22:04:49 -0800

    ADS LAB

---
+++ TOPOLOGI.C
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+ int i,j,k,n,a[10][10],indeg[10],flag[10],count=0;
+ clrscr();
+ printf("\n enter the number of vertices:");
+ scanf("%d",&n);
+ printf("enter the adjacency matrix:\n");
+ for(i=0;i<n;i++)
+ {
+   printf("enter the row %d\n",i+1);
+   for(j=0;j<n;j++)
+   scanf("%d",&a[i][j]);
+ }
+ for(i=0;i<n;i++)
+ {
+  indeg[i]=0;
+  flag[i]=0;
+ }
+ for(i=0;i<n;i++)
+ for(j=0;j<n;j++)
+ indeg[i]=indeg[i]+a[j][i];
+ printf("\n the topological order:");
+ while(count<n)
+ {
+   for(k=0;k<n;k++)
+   {
+     if((indeg[k]==0)&&(flag[k]==0))
+     {
+      printf("%d",(k+1));
+      flag[k]=1;
+     }
+     for(i=0;i<n;i++)
+     {
+      if(a[i][k]==1)
+      indeg[k]--;
+     }
+   }
+   count++;
+ }
+ getch();
+ return 0;
+}
